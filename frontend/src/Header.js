e9693056c3ef2e1f16b57db980897fe3b39869c0
K.Thonglek <kundjanasith.t@ku.th>
2025-12-20 22:49:13 +0700

    update header

--- frontend/src/Header.js
+++ frontend/src/Header.js
@@ -5,7 +5,8 @@ function Header() {
   return (
     <header style={{paddingBottom: 0, margnBottom: 0, paddingTop: 0}} className="Header">
       <img src={process.env.PUBLIC_URL + "/simatec_shadow02.png"} alt="Simatec Logo" className="white-shadow"/>
-      <h1>AI-powered Molecular Modelling R&D Service Solution</h1>
+      {/* <h1>AI-powered Molecular Modelling R&D Service Solution</h1> */}
+      <h1>SIMATEC : Digital Chemistry R & D</h1>
     </header>
   );
 }
